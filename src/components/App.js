e670d799fbd47d8a210796df26c6b40c3fcd3618
Subrata Sarker <subrata_sarker@yahoo.com>
2019-03-31 20:42:00 +0900

    Mutations: Creating link

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import LinkList from './LinkList';
 import '../styles/App.css';
+import CreateLink from './CreateLink';
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return <CreateLink />
   }
 }
 
