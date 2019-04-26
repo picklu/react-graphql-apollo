d970fdc7de5bc7e8462f90486f803e3c57a1fdaf
Subrata Sarker <subrata_sarker@yahoo.com>
2019-04-26 23:12:25 +0900

    Searching the links

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,13 @@ class Header extends Component {
     return (
       <div className="flex pa1 justify-between nowrap orange">
         <div className="flex flex-fixed black">
-          <div className="fw7 mr1">Hacker News</div>
+          <div className="fw7 mr1">Hacker News:</div>
           <Link to="/" className="ml1 no-underline black">
             new
+          </Link>
+              <div className="ml1">|</div>
+          <Link to="/search" className="ml1 no-underline black">
+            search
           </Link>
           {authToken && (
             <div className="flex">
