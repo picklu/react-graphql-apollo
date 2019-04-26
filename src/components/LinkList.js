19e60f62569d242e1529d640acd5cf85229fcaf4
Subrata Sarker <subrata_sarker@yahoo.com>
2019-04-26 12:07:02 +0900

    Updating the store

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,21 +3,39 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import Link from './Link';
 
-const FEED_QUERY = gql`
-    {
-        feed {
-            links {
-                id
-                createdAt
-                url
-                description
-            }
+export const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
         }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
     }
+  }
 `
 
-
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+  
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+  
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
   render() {
     return (
       <Query query={FEED_QUERY}>
@@ -26,10 +44,16 @@ class LinkList extends Component {
           if (error) return <div>Error</div>
     
           const linksToRender = data.feed.links
-    
           return (
             <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+              {linksToRender.map((link, index) => (
+               <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
+              ))}
             </div>
           )
         }}
